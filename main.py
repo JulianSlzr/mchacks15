9b55af4eb1d40517fce310b0751713dc8448f13f
Neil Parikh <parikh.neil@me.com>
2015-02-21 21:47:20 -0500

    Add unique id for each wav file

--- main.py
+++ main.py
@@ -4,11 +4,16 @@ import gevent
 from flask import Flask, render_template, url_for, redirect
 from flask_sockets import Sockets
 import io
+import string
+import random
 
 app = Flask(__name__)
 path = os.getcwd()
 app.config['DEBUG'] = True
 sockets = Sockets(app)
+
+def rand_id(size=8):
+    return ''.join(random.SystemRandom().choice(string.ascii_uppercase + string.digits) for _ in range(size))
 
 @app.route('/', methods=['GET', 'POST'])
 def main():
@@ -16,11 +21,13 @@ def main():
 
 @sockets.route('/submit')
 def submit(ws):
+    user_id = rand_id()
     while not ws.closed:
         gevent.sleep()
         data = ws.receive()
-        if data:
-            ws.send("ayy lmao")
-            wave_file = io.open("test.wav", "wb")
+        if data and data != "start":
+            file_name = "_".join(["file", user_id, rand_id()])
+            wave_file = io.open(file_name, "wb")
             wave_file.write(data)
             wave_file.close()
+            # process_file(file_name)
