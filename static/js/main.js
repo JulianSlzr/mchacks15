cb0504a5f9419a00d6d7077e80755ff5dbdbb07e
Julian Salazar <julian@slzr.me>
2015-02-22 06:19:31 -0500

    metronome and canvas extension

--- static/js/main.js
+++ static/js/main.js
@@ -7,14 +7,45 @@
 
 		// ==== RENDER THE STAFF OBJECT ====
 
-		var canvas = $("#staff")[0];
-		var renderer = new Vex.Flow.Renderer(canvas,
-		Vex.Flow.Renderer.Backends.CANVAS);
+		var staveCount = 0
 
-		var ctx = renderer.getContext();
-		var stave = new Vex.Flow.Stave(10, 0, 700);
-		stave.addClef("treble").setContext(ctx).draw();
+		function createNewStave() {
 
+			$(".countdown-row").append('<canvas class="staff" width="700" height="180"></canvas>');
+			var canvas = $(".staff")[staveCount];
+			var renderer = new Vex.Flow.Renderer(canvas,
+			Vex.Flow.Renderer.Backends.CANVAS);
+
+			var ctx = renderer.getContext();
+			var bar1_treble;
+			var bar1_bass;
+			var bar2_treble;
+			var bar2_bass;
+
+			// bar 1
+			var bar1_treble = new Vex.Flow.Stave(20, 0, 330);
+			bar1_treble.addClef("treble");
+			bar1_treble.setContext(ctx).draw();
+
+			var bar1_bass = new Vex.Flow.Stave(20, 80, 330);
+			bar1_bass.addClef("bass");
+			bar1_bass.setContext(ctx).draw();
+
+			var brace = new Vex.Flow.StaveConnector(bar1_treble, bar1_bass);
+			brace.setType(Vex.Flow.StaveConnector.type.BRACE);
+			brace.setContext(ctx).draw();
+
+			// bar 2 - juxtaposing second bar next to first bar
+			var bar2_treble = new Vex.Flow.Stave(bar1_treble.width + bar1_treble.x, bar1_treble.y, 330);
+			bar2_treble.setContext(ctx).draw();
+			var bar2_bass = new Vex.Flow.Stave(bar1_bass.width + bar1_bass.x, bar1_bass.y, 330);
+			bar2_bass.setContext(ctx).draw();
+
+			staveCount++;
+
+		}
+
+		createNewStave()
 
 		// ==== FREQUENCY VIEWER ====
 
@@ -97,6 +128,60 @@
 		navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia;
 		navigator.getUserMedia({audio:true}, gotStream, function() {alert('error')});
 
+		// // ==== METRONOME CODE ====
+
+    	var ticker;
+    	var interval = spinner.value / 60.0;
+    	var noteLength = 0.05;
+    	// var tempo = 120.0;
+    	var tickerCount = 0;
+
+    	function metronome() {
+			ticker = setInterval(function() {
+
+				// create an oscillator
+		        var osc = context.createOscillator();
+		        osc.connect( context.destination );
+
+				if (tickerCount % 4 == 0) {
+					$('.beep').css("background-color", "orange");
+					osc.frequency.value = 880.0;
+				} else {
+					$('.beep').css("background-color", "red");
+					osc.frequency.value = 440.0;
+				}
+
+				setTimeout(function() {
+					$('.beep').css("background-color", "black")
+				}, 200)
+
+				if (tickerCount < 8) {
+
+			        osc.start( context.currentTime );
+			        osc.stop( context.currentTime + noteLength );
+
+			        $('#countdown span').html(8 - tickerCount);
+			        $('#countdown span').css("background-color", "rgba(256, 256, 256, 0.7)");
+
+			    } else {
+
+			    	$('#countdown').css("visibility", "hidden");
+			    	startRecord();
+
+			    	if (tickerCount >= 16 && tickerCount % 8 == 0) {
+			    		console.log("got here")
+			    		createNewStave();
+			    	}
+
+			    }
+
+		        tickerCount++;
+
+		        // FLASH THINGY
+
+		    }, (60.0 / $("#bpm").val()) * 1000);
+    	}
+
 		// ==== SET UP WEBSOCKET THINGY ====
 
 		// CHANGE TO RELATIVE URL
@@ -106,7 +191,7 @@
 		};
 
 		ws.onmessage = function(e) {
-		   console.log("ayy lmao")
+		   console.log(e.data)
 		}
 
 		// ==== SET UP MIC HANDLER ====
@@ -114,7 +199,16 @@
 		var intervalKey;
 		var rec;
 
+		var hasStarted = false; 
+
 		function startRecord() {
+
+			if (hasStarted)
+				return;
+
+			hasStarted = true;
+
+			console.log('a');
 
 			rec = new Recorder(sourceNode);
 
@@ -132,41 +226,71 @@
 
 		}
 
+
     	$('i.fa-microphone').click(function() {
+
 
     		if ($(this).hasClass('recording')) {
     			console.log('b');
     			rec.stop();
 				rec.clear();
+				clearInterval(ticker);
 				clearInterval(intervalKey);
     		} else {
-    			console.log('a')
-    			startRecord();
+    			metronome();
     		}
 
     		$(this).toggleClass('recording');
 
-			// var notes = [
-			//     // A quarter-note C.
-			//     new Vex.Flow.StaveNote({ keys: ["c/4"], duration: "q" }),
-			//     new Vex.Flow.StaveNote({ keys: ["d/4"], duration: "q" }),
-			//     new Vex.Flow.StaveNote({ keys: ["e/4"], duration: "q" }),
-			//     new Vex.Flow.StaveNote({ keys: ["f/4"], duration: "q" })
-			// ]
+    		var notesTreble = [
+			  new Vex.Flow.StaveNote({ keys: ["e/5"], duration: "q" }),
+			  new Vex.Flow.StaveNote({ keys: ["d/5"], duration: "h" }),
+			  new Vex.Flow.StaveNote({ keys: ["c/5", "e/5", "g/5"], duration: "q" })
+			];
 
-			// // Create a voice in 4/4
-			//   var voice = new Vex.Flow.Voice({
-			//     num_beats: 4,
-			//     beat_value: 4,
-			//     resolution: Vex.Flow.RESOLUTION
-			//   });
+			var notesBass = [
+			  new Vex.Flow.StaveNote({ keys: ["c/3"], duration: "h", clef: "bass" }),
+			  new Vex.Flow.StaveNote({ keys: ["g/3"], duration: "h", clef: "bass" })
+			];
 
-			//   // Add notes to voice
-			//   voice.addTickables(notes);
+			function create_4_4_voice(staff) {
+			  return new Vex.Flow.Voice({
+			    num_beats: 4,
+			    beat_value: 4,
+			    resolution: Vex.Flow.RESOLUTION
+			  });
+			}
 
-			//   // Format and justify the notes to 700 pixels
-			//   var formatter = new Vex.Flow.Formatter().
-			//     joinVoices([voice]).format([voice], 700);
+			// var voiceTreble = create_4_4_voice().addTickables(notesTreble);
+			// var voiceBass = create_4_4_voice().addTickables(notesBass);
+
+			// var formatter = new Vex.Flow.Formatter()
+			//   .joinVoices([voiceTreble, voiceBass])
+			//   .format([voiceTreble, voiceBass], 330);
+
+			// voiceTreble.draw(ctx, bar1_treble);
+			// voiceBass.draw(ctx, bar1_bass);
+
+			// var notesBar1 = [
+			// new Vex.Flow.StaveNote({ keys: ["c/4"], duration: "q" }),
+			// new Vex.Flow.StaveNote({ keys: ["d/4"], duration: "q" }),
+			// new Vex.Flow.StaveNote({ keys: ["b/4"], duration: "qr" }),
+			// new Vex.Flow.StaveNote({ keys: ["c/4", "e/4", "g/4"], duration: "q" })
+			// ];
+
+			// // Helper function to justify and draw a 4/4 voice
+			// Vex.Flow.Formatter.FormatAndDraw(ctx, staveBar1, notesBar1);
+
+			// var notesBar2 = [
+			// new Vex.Flow.StaveNote({ keys: ["c/4"], duration: "8" }),
+			// new Vex.Flow.StaveNote({ keys: ["d/4"], duration: "8" }),
+			// new Vex.Flow.StaveNote({ keys: ["g/4"], duration: "8" }),
+			// new Vex.Flow.StaveNote({ keys: ["e/4"], duration: "8" })
+			// ];
+
+			// // Helper function to justify and draw a 4/4 voice 
+			// Vex.Flow.Formatter.FormatAndDraw(ctx, staveBar2, notesBar2);
+
 
 			// setTimeout(function() {
 			// 	// Render voice
